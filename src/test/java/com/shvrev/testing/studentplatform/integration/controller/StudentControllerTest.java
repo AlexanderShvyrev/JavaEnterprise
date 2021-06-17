c37202b0cb3884e7ab44a2ece0435312cae06287
Alexander Shvyrev <shurik_stv@yahoo.com>
2021-06-17 18:23:57 -0400

    just another commit

--- src/test/java/com/shvrev/testing/studentplatform/integration/controller/StudentControllerTest.java
+++ src/test/java/com/shvrev/testing/studentplatform/integration/controller/StudentControllerTest.java
@@ -14,37 +14,58 @@ import org.springframework.http.*;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
 import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import static com.shvrev.testing.studentplatform.integration.common.TestUtils.getClassPathResourceAsObject;
 
 
 @ActiveProfiles("test")
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = {StudentPlatformApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StudentControllerTest extends ContainerEnv {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
     @Test
-    public void createStudentTest(){
-        StudentDTO createdStudentDTO = createNewStudent("student1.json");
-        assertEquals(true, true);
+    public void createStudentTest() {
+        createNewStudent("student1.json");
+        List<StudentDTO> allStudents = getAllStudents();
+        assertEquals(1,allStudents.size());
+        assertEquals("BarakBarak", allStudents.get(0).getFirstName());
+        assertEquals("Obamabama", allStudents.get(0).getLastName());
+        assertEquals(LocalDate.of(1999,2,2), allStudents.get(0).getBirthDate());
+
     }
 
-    private StudentDTO createNewStudent(String filename) {
-        StudentDTO studentDto = getClassPathResourceAsObject("/dto/students/" + filename, new TypeReference<>() {});
+    private void createNewStudent(String filename) {
+        StudentDTO studentDto = getClassPathResourceAsObject("/dto/students/" + filename, new TypeReference<>() {
+        });
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        ResponseEntity<StudentDTO> result = testRestTemplate.exchange(
+        testRestTemplate.exchange(
                 "/api/students",
                 HttpMethod.POST,
                 new HttpEntity<>(studentDto, HttpHeaders.writableHttpHeaders(headers)),
                 new ParameterizedTypeReference<>() {
                 }
         );
-        return result.getBody();
+    }
+
+    private List<StudentDTO> getAllStudents() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        ResponseEntity<List<StudentDTO>> list = testRestTemplate.exchange(
+                "/api/students",
+                HttpMethod.GET,
+                new HttpEntity<>(headers),
+                new ParameterizedTypeReference<>() {
+                }
+        );
+        return list.getBody();
     }
 }
